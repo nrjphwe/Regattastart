957225d6d0f8d90fc24af186829f178f2ad767cc
nrjphwe <philip@werner.nu>
2020-11-24 21:52:48 +0000

    add

---
+++ script/install_power.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo "=> Installing apache...\n"
+sudo apt update
+sudo apt install apache2 -y
+sudo a2enmod cgi
+
+
+echo "=> Installing regattastart files at CGI-BIN...\n"
+sudo cp -v power_check.py /usr/lib/cgi-bin
+sudo chmod -R 755 /usr/lib/cgi-bin
+sudo chown -R www-data:www-data /usr/lib/cgi-bin
+
+echo "=> Installing PHP...\n"
+sudo apt install php libapache2-mod-php -y
+
+echo "=> Installing regattastart php files at /var/www/html/...\n"
+sudo cp -v w3.css /var/www/html
+sudo chmod -R 755 /var/www/html/
+sudo chown -R www-data:www-data /var/www/html
+
+echo "=> setup for ADS1115 ...\n"
+pip3 install adafruit-circuitpython-ads1x15
+
+
+echo "=> setup SQL-Mariadb:...\n"
+sudo apt install mariadb-server
+sudo apt-get install -y php5-mysql
+sudo systemctl stop mariadb
+sudo mysqld_safe --skip-grant-tables --skip-networking &
+echo mysql -u root
+sudo mysql_secure_installation
+sudo systemctl start mariadb
+systemctl status mariadb.service
---
+++ script/install_regattastart.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo "=> Installing apache...\n"
+sudo apt update
+sudo apt install apache2 -y
+sudo a2enmod cgi
+
+
+echo "=> Installing regattastart files at CGI-BIN...\n"
+sudo cp -v regattastart7.py /usr/lib/cgi-bin
+sudo cp -v regattastart6.py /usr/lib/cgi-bin
+sudo cp -v select_data7.py /usr/lib/cgi-bin
+sudo cp -v select_data6.py /usr/lib/cgi-bin
+sudo cp -v logging.conf /usr/lib/cgi-bin/
+sudo cp -v logging.conf /usr/lib/cgi-bin/
+sudo cp -v dropbox_uploader.sh /usr/lib/cgi-bin/
+sudo cp -vr Dropbox-Uploader /usr/lib/cgi-bin/
+sudo chmod -R 755 /usr/lib/cgi-bin
+sudo chown -R www-data:www-data /usr/lib/cgi-bin
+
+echo "=> Installing PHP...\n"
+sudo apt install php libapache2-mod-php -y
+
+echo "=> Installing regattastart php files at /var/www/html/...\n"
+sudo mkdir -v -p /var/www/html/images
+sudo cp -v /var/www/html/index.html /var/www/html/index0.html
+sudo rm -v /var/www/html/index.html
+sudo cp -v index.php /var/www/html
+sudo cp -v index0.php /var/www/html
+sudo cp -v index6.php /var/www/html
+sudo cp -v index7.php /var/www/html
+sudo cp -v w3.css /var/www/html
+sudo chmod -R 755 /var/www/html/
+sudo chown -R www-data:www-data /var/www/html
+
+echo "=> setup for videocamera ...\n"
+sudo usermod -a -G video www-data
+sudo chmod 777 /dev/vchiq
+sudo chown root.gpio /dev/gpiomem
+sudo chmod g+rw /dev/gpiomem
+sudo usermod -a -G gpio www-data
+
+echo "=> setup for video encoding...\n"
+sudo apt install -y gpac
