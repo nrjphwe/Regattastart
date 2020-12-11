8e0eb4d80966c9a1a803679bad2d59c5c9f6f874
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2020-12-11 10:06:18 +0100

    Update install_regattastart.sh
    moved some pats into central_install.sh

--- script/install_regattastart.sh
+++ script/install_regattastart.sh
@@ -4,11 +4,6 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-echo "=> Installing apache...\n"
-sudo apt update
-sudo apt install apache2 -y
-sudo a2enmod cgi
-
 echo "=> Installing regattastart files at CGI-BIN...\n"
 sudo cp -v regattastart7.py /usr/lib/cgi-bin
 sudo cp -v regattastart6.py /usr/lib/cgi-bin
@@ -20,9 +15,6 @@ sudo cp -v dropbox_uploader.sh /usr/lib/cgi-bin/
 sudo cp -vr Dropbox-Uploader /usr/lib/cgi-bin/
 sudo chmod -R 755 /usr/lib/cgi-bin
 sudo chown -R www-data:www-data /usr/lib/cgi-bin
-
-echo "=> Installing PHP...\n"
-sudo apt install php libapache2-mod-php -y
 
 echo "=> Installing regattastart php files at /var/www/html/...\n"
 sudo mkdir -v -p /var/www/html/images
@@ -45,16 +37,3 @@ sudo usermod -a -G gpio www-data
 
 echo "=> setup for video encoding...\n"
 sudo apt install -y gpac
-
-# We have a script "listen-for-shutdown.py", which will shutdown the PI when triggered by a switch.  
-# We need to start this script on boot. So we'll place the script in /usr/local/bin and make it executable:
-sudo cp listen-for-shutdown.py /usr/local/bin/
-sudo chmod +x /usr/local/bin/listen-for-shutdown.py
-
-# Now add another script called listen-for-shutdown.sh that will start/stop our service.
-# Place this file in /etc/init.d and make it executable.
-sudo cp listen-for-shutdown.sh /etc/init.d/
-sudo chmod +x /etc/init.d/listen-for-shutdown.sh
-# Now we'll register the script to run on boot.
-sudo update-rc.d listen-for-shutdown.sh defaults
-
