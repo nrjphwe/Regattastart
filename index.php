15b2a8f4a625e761a83e891f65ce6cfe4c201f2c
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-12-21 22:48:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -96,8 +96,6 @@ if (file_exists($filename))
 <h4><a href="/index6.php" title="Version using sessions"> Regattstart6 setup </a></h4>
 <div align="center">
 <h4><a href="/index7.php" title="Version using sessions"> Regattstart7 setup </a></h4>
-// <div align="center">
-// <h4><a href="http://90.236.109.106:3000" title="Grafana data and charts"> Grafana Dashboard</a></h4>
 <div align="center">
 <h3> Bilder tagna vid varje signal innan 1a start </h3>
 <h3> Bild vid varningssignal 5 minuter innan 1a start</h3>
