ac63278724f9ca3dcd7710636ffeb4068b9efbcd
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2022-05-01 22:52:06 +0200

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -263,7 +263,7 @@ while ( True ):
 # https://stackoverflow.com/questions/45040261/python-3-auto-conversion-from-h264-to-mp4
 #---------------------------------------------------------------------------------------#
                     try:
-                        output = subprocess.run(convert_video, shell=True)
+                        output = subprocess.run(convert_video, shell=True, check=True)
                     except subprocess.CalledProcessError as e:
                         logger.info ('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
                         logger.info (e)
@@ -331,7 +331,7 @@ while ( True ):
                         logger.info (" convert video %s to mp4 format", i)
                         convert_video = "MP4Box " + "-add " + photo_path + "video" + str(i) + ".h264 " + photo_path + "video" + str(i) + ".mp4" + "-v"
                         try:
-                            output = subprocess.run(convert_video, shell=True)
+                            output = subprocess.run(convert_video, shell=True, check=True)
                         except subprocess.CalledProcessError as e:
                             logger.info ('FAIL:\ncmd:{}\output:{}'.format(e.cmd, e.output))
                         logger.info (" video%s converted to mp4 format", i)
