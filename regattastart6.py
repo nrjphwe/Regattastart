e0d58c71a8f0214daf5b2f1dbb12c914f36782fc
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2022-05-16 14:54:12 +0200

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -24,7 +24,7 @@
 #--------------------------------------------------------------------#")
 import os, sys
 import cgitb
-cgitb.enable(display=0, logdir="/var/www/html/images")
+cgitb.enable(display=0, logdir="/tmp")
 start_time = str(sys.argv[1])
 week_day = str(sys.argv[2])
 video_delay = int(sys.argv[3])
