22935da28ba50734e1df8280cd78afe2d13f50da
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-03-28 20:56:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -152,6 +152,7 @@ for ($x = 1; $x <= $num_video; $x++)
     echo "<h2> Finish video, this is video $x for the finish</h2><br>";
     $video_name =  "/images/video" . $x . ".mp4";
     echo "<video src=" . $video_name . " controls ></video><p>";
+    echo "<video width = "720" height="480" controls><source src= " . $video_name . "type="video/mp4"></video><p>";
 }
 ?>
 </div>
