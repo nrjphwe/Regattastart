689b771bd8ca6da47356dcd8e8acdec6f6d9cf6b
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-19 17:14:36 +0200

    Update select_data6.py

--- select_data6.py
+++ select_data6.py
@@ -3,7 +3,9 @@
 import time, subprocess, sys, os 
 from http import cookies
 import cgi, cgitb,datetime
+
 cgitb.enable(display=0, logdir="/var/log") #for debugging
+
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
@@ -17,23 +19,40 @@ Passing cookies between a .cgi and php program
 """
 cookme = cookies.SimpleCookie()
 cookme.load(os.environ.get('HTTP_COOKIE',''))
-##
-#cookme = os.environ.get('HTTP_COOKIE')
-##
+
+# Create a new FieldStorage instance
 FormData = cgi.FieldStorage()
 entered_name=FormData.getvalue('video_delay')
 test = 0
-#to set a cookie this has to be your first output.
-if ('video_delay'or 'video_dur') in FormData:
+
+# To set a cookie this has to be your first output.
+if 'video_delay' in FormData or 'video_dur' in FormData:
     cooked = cookies.SimpleCookie()
-    cooked['video_delay']=FormData.getvalue('video_delay')
-    cooked['video_delay']['path']='/'
-    cooked['video_dur']=FormData.getvalue('video_dur')
-    cooked['video_dur']['path']='/'
-    cooked['num_video']=FormData.getvalue('num_video')
-    cooked['num_video']['path']='/'
-    cooked['start_time']=FormData.getvalue('start_time')
-    cooked['start_time']['path']='/'
+    if 'video_delay' in FormData:
+        cooked['video_delay'] = FormData.getvalue('video_delay')
+        cooked['video_delay']['path'] = '/'
+    if 'video_dur' in FormData:
+        cooked['video_dur'] = FormData.getvalue('video_dur')
+        cooked['video_dur']['path'] = '/'
+    if 'num_video' in FormData:
+        cooked['num_video'] = FormData.getvalue('num_video')
+        cooked['num_video']['path'] = '/'
+    if 'start_time' in FormData:
+        cooked['start_time'] = FormData.getvalue('start_time')
+        cooked['start_time']['path'] = '/'
+
+#if ('video_delay'or 'video_dur') in FormData:
+#    cooked = cookies.SimpleCookie()
+#    cooked['video_delay']=FormData.getvalue('video_delay')
+#    cooked['video_delay']['path']='/'
+#    cooked['video_dur']=FormData.getvalue('video_dur')
+#    cooked['video_dur']['path']='/'
+#    cooked['num_video']=FormData.getvalue('num_video')
+#    cooked['num_video']['path']='/'
+#    cooked['start_time']=FormData.getvalue('start_time')
+#    cooked['start_time']['path']='/'
+
+
     test = 1
     print (cooked)
 # Get data from fields
@@ -42,11 +61,11 @@ print ()
 print ("<html>")
 print ("<head>")
 print ("<title> select_data6.py Regattastart6 sessions </title>")
+print("</head>")
 print ("<body>")
-print("Debug: Script is running")  # Add this line
-#print ("<meta http-equiv='refresh' content='60; URL=/cgi-bin/select_data6.py'>")
+
 try:
-      #week_day = (form.getvalue('day'))
+      week_day = (form.getvalue('day'))
       start_time = (form.getvalue('start_time'))
       num_video = int(form.getvalue('num_video'))
       video_delay = int(form.getvalue('video_delay'))
@@ -55,6 +74,7 @@ try:
 except:
       print ("<p>Sorry, we cannot turn your input to numbers.<p/>")
 ###
+print("Debug: Script is running")  # Add this line
 try:
     # Parsing and other logic
     # Debugging output for values obtained from the form data
@@ -66,7 +86,7 @@ except ValueError:
 # ...
 print("Debug: Reached the end of the script")  # Add this line at the end of the script
 
-if ('video_delay' or 'video_dur') in cookme:
+if ('video_delay') in cookme:
     print ("<h4>Previous or current video_delay:")
     print (cookme.get('video_delay').value)
     print ("Current video_dur:")
@@ -79,6 +99,7 @@ if ('video_delay' or 'video_dur') in cookme:
     print ("</h4>")
 else:
     print ("<h4>no video_delay/video_dur set yet.</h4>")
+   
 if test==1:
     print ("<h4>your cookies have been changed just now.</h4><br/>")
     print ("<h4>new video_delay = ")
@@ -88,6 +109,7 @@ if test==1:
     print ("</h3><br/>")
 else:
     print ("No video_delay or video_dur were specified so it is not being changed.<br/>")
+   
 print ("<form method=\"post\" action = \"/index6.php\" name='myform1'>")
 print ("video_delay: <input name='video_delay' size=3 /><br/>")
 print ("video_dur....: <input name='video_dur' size=3 /><br/>")
@@ -95,9 +117,12 @@ print ("num_video....: <input name='num_video' size=3 /><br/>")
 print ("then we we'll set a cookie and everybody is happy.<br/>")
 print ("<input type='submit'/>")
 print ("</form>")
+
 print ("<h2> <a href=""/index.php"">  Resultat sida  </a></h2>")
+
 print("</body>")
 print("</html>")
+
 sys.stdout.flush()
 os.close(sys.stdout.fileno()) # Break web pipe
 
