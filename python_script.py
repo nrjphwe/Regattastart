9e60173352e146bd08bf2541cbbb19d785253c0b
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-22 20:07:57 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -10,13 +10,15 @@ print("Content-Type: text/html\n")
 import os
 from http import cookies
 
-cookie = cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
-user_name = cookie.get('user_name').value if 'user_name' in cookie else "Name not set"
-
-#Debug:
 print(f"Cookie Data: {os.environ.get('HTTP_COOKIE')}")
 print(f"User's Name (from session): {user_name}")
 # Process the user data
 print(f"<html><body>")
 print(f"User's Name: {user_name}")
+
+
+#Debug:
+cookie = cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+user_name = cookie.get('user_name').value if 'user_name' in cookie else "Name not set"
+print(f"debug_User's Name") 
 print(f"</body></html>")
