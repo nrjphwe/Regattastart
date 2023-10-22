57a77d20f866d89e52034641e8de7c83e98453de
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-22 14:59:04 +0200

    Update php_web.php

--- php_web.php
+++ php_web.php
@@ -3,7 +3,7 @@ session_start(); // Start the session
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_SESSION['user_name'] = $_POST['name'];
-    header('Location: python_script.php'); // Redirect to the Python script
+    header('Location: python_script.py'); // Redirect to the Python script
     exit();
 }
 ?>
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>PHP to Python</title>
 </head>
 <body>
-    <form method="post" action="/cgi-bin/python_script.php">
+    <form method="post" action="/cgi-bin/python_script.py">
         <label for="name">Name:</label>
         <input type="text" name="name" id="name">
         <input type="submit" value="Submit">
