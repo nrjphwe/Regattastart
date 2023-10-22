33ae4bce863091fbfce2231a988e168c535464dc
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-22 15:11:13 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-import requests
+import cgi
+import cgitb
+cgitb.enable()  # Enable CGI error reporting
 
-php_url = 'http://regattastart.local/php_web.php'
-response = requests.get(php_url)
-cookies = response.cookies
-user_name = requests.get(php_url, cookies=cookies).text
+# Print the Content-Type header
+print("Content-Type: text/html\n")
 
+# Read user data from the session
+import os
+from http import cookies
+
+cookie = cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+user_name = cookie.get('user_name').value if 'user_name' in cookie else "Name not set"
+
+# Process the user data
+print(f"<html><body>")
 print(f"User's Name: {user_name}")
+print(f"</body></html>")
