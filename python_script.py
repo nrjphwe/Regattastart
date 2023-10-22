afe61d7e6163befad02cb9458e85360790444ba3
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-22 20:30:26 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+
 import cgi
 import cgitb
+
 cgitb.enable()  # Enable CGI error reporting
 
 # Print the Content-Type header
 print("Content-Type: text/html\n")
 
-# Read user data from the session
+# Read user data from the request
+import sys
 import os
-from http import cookies
 
-cookie = cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
-user_name = cookie.get('user_name').value if 'user_name' in cookie else "Name not set"
+form = cgi.FieldStorage()
+user_name = form.getvalue("name")
 
-# Process the user data
-print(f"<html><body>")
-print(f"User's Name (from session): {user_name}")
-print(f"</body></html>")
-
-print(f"Cookie Data: {os.environ.get('HTTP_COOKIE')}")
-print(f"User's Name (from session): {user_name}")
+if user_name:
+    print(f"<html><body>")
+    print(f"User's Name (from request): {user_name}")
+    print(f"</body></html>")
+else:
+    print(f"<html><body>")
+    print("User's Name not provided in the request.")
+    print(f"</body></html>")
