03cc11780b3f16bf58441fb72af21e937e2cb9d1
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-24 17:25:26 +0200

    Update select_data6.py

--- select_data6.py
+++ select_data6.py
@@ -21,7 +21,6 @@ start_time = (form.getvalue('start_time'))
 num_video = int(form.getvalue('num_video'))
 video_delay = int(form.getvalue('video_delay'))
 video_dur = int(form.getvalue('video_dur'))
-time.sleep(1)
 execution_string =  "python3 " + "regattastart6.py " + str(start_time) + " " + week_day + " " + str(video_delay) + " " + str(num_video) + " " + str(video_dur) + " " + " &"
 proc = subprocess.run([execution_string], shell = True)
 
