5d2dd11a9f344fe7dd14e91d36606a7ee2bde3dd
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-24 15:23:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -126,42 +126,42 @@ $num_video = isset($_SESSION['num_video']) ? $_SESSION['num_video'] : '';
 <h3> Foto vid 1a start</h3>
 <img src="/images/1st-start_pict.jpg?<?php echo Date("Y.m.d.G.i.s")?>" alt="1st start  picture" width="720" height="480"  >
 <?php
-$path = '/images/';
-$filename = 'images/2nd-5min_pict.jpg';
-if (file_exists($filename)) {
-   echo "<h3> Bilder tagna vid varje signal innan 2a start  </h3> ";
-   $filename = '2nd-5min_pict.jpg';
-   echo "<h3> Bild vid varningssignal 5 minuter innan 2a start</h3>";
-   echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
-   $filename = '2nd-4min_pict.jpg';
-   echo "<h3> Signal 4 minuter innan 2a start </h3>";
-   echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
-   $filename = '2nd-1min_pict.jpg';
-   echo "<h3> Signal 1 minut innan 2a start </h3>";
-   echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
-   $filename = '2nd-start_pict.jpg';
-   echo "<h3> Foto vid 2a start </h3>";
-   echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
-}
+    $path = '/images/';
+    $filename = 'images/2nd-5min_pict.jpg';
+    if (file_exists($filename)) {
+       echo "<h3> Bilder tagna vid varje signal innan 2a start  </h3> ";
+       $filename = '2nd-5min_pict.jpg';
+       echo "<h3> Bild vid varningssignal 5 minuter innan 2a start</h3>";
+       echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
+       $filename = '2nd-4min_pict.jpg';
+       echo "<h3> Signal 4 minuter innan 2a start </h3>";
+       echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
+       $filename = '2nd-1min_pict.jpg';
+       echo "<h3> Signal 1 minut innan 2a start </h3>";
+       echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
+       $filename = '2nd-start_pict.jpg';
+       echo "<h3> Foto vid 2a start </h3>";
+       echo '<img src = "' . $path . $filename . '" / alt="1st 5 min picture" width="720" height="480"  >';
+    }
 ?>
 <div class="w3-panel w3-pale-blue">
 <?php
-$video_name = '/images/video0.mp4';
-{
-   echo "<h3> Video 5 min före start och 2 min efter, eller vid 2 starter, till 2 min efter andra start </h3>";
- //  echo "<video src = " . $video_name . " controls ></video><p>";
-   echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
-}
+    $video_name = '/images/video0.mp4';
+    {
+       echo "<h3> Video 5 min före start och 2 min efter, eller vid 2 starter, till 2 min efter andra start </h3>";
+       echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
+    }
 ?>
 </div>
+<div class="w3-panel w3-pale-red">
 <?php
-for ($x = 1; $x <= $num_video; $x++)
-{
-    echo "<h2> Finish video, this is video $x for the finish</h2><br>";
-    $video_name =  "/images/video" . $x . ".mp4";
-//    echo "<video src=" . $video_name . " controls ></video><p>";
-    echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
-}
+    for ($x = 1; $x <= $num_video; $x++)
+    {
+        echo "<h2> Finish video, this is video $x for the finish</h2><br>";
+        $video_name =  "/images/video" . $x . ".mp4";
+    //    echo "<video src=" . $video_name . " controls ></video><p>";
+        echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
+    }
 ?>
 </div>
 </main>
