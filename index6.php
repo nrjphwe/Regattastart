36634b8aac0183a6fc2a6be601f5af8cffd41a5f
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-25 19:30:20 +0200

    Update index6.php

--- index6.php
+++ index6.php
@@ -176,7 +176,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div id="submit" align="center"></div>
         <div class="w3" align="center">
         <br>
-        <input type = "submit" value = "Submit"/>
+        <input type = "submit" value = "Submit"/input>
     </form>
 </div>          
 <!-- Here is our page's main content -->
