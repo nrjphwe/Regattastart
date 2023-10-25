f2d582acd76e1d87f000fde33572a34402a59187
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-25 14:12:57 +0200

    Update select_data6.py

--- select_data6.py
+++ select_data6.py
@@ -22,24 +22,32 @@ start_time = (form.getvalue('start_time'))
 num_video = int(form.getvalue('num_video'))
 video_delay = int(form.getvalue('video_delay'))
 video_dur = int(form.getvalue('video_dur'))
-execution_string =  "python3 " + "regattastart6.py " + str(start_time) + " " + week_day + " " + str(video_delay) + " " + str(num_video) + " " + str(video_dur) + " " + " &"
-proc = subprocess.run([execution_string], shell = True)
 
-# Process the form data
-# ...
-# Send a response back to index6.php
-response = {"message": "Data processed successfully"}
-print("Content-Type: application/json")
-print()
-print(json.dumps(response))
-
-
-if week_day:
-   print(f"<html><body>")
-   print (f"Start is set to: {start_time}")
-   print (f"Number of videos: {num_video}")
-   print(f"</body></html>")
+# Check for None values and convert to the appropriate types
+if not (week_day and start_time and num_video and video_delay and video_dur):
+    print("<html><body>")
+    print("Error: Some fields are missing.")
+    print("</body></html>")
 else:
-    print(f"<html><body>")
-    print (f"31 Sorry, we cannot turn your input to numbers.</p>")
-    print(f"</body></html>")
+   try:
+      start_time = str(start_time)
+      num_video = int(num_video)
+      video_delay = int(video_delay)
+      video_dur = int(video_dur)
+      execution_string = (
+         "python3 "
+         "regattastart6.py "
+         f"{start_time} {week_day} {video_delay} {num_video} {video_dur} &"
+         )
+        proc = subprocess.run([execution_string], shell=True)
+   # Process the form data
+   # ...
+   # Send a response back to index6.php
+   response = {"message": "Data processed successfully"}
+        print("Content-Type: application/json")
+        print()
+        print(json.dumps(response))
+    except ValueError:
+        print("<html><body>")
+        print("Error: Some fields contain invalid values.")
+        print("</body></html>")
