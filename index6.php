c249ca129f53b499d1f4a509d5196a5e10f60f43
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-25 11:37:44 +0200

    Update index6.php

--- index6.php
+++ index6.php
@@ -153,7 +153,7 @@ error_log("Check if the form has been submitted");
     <form action="http://regattastart.local/" method="POST">
           <div>
             <label for="num_video">Number of videos?</label>
-            <input type="hidden" name="num_video" id="hidden_field_num_video" value="<?php echo $_POST['num_video'] ?>" />
+            <input type="hidden" name="num_video" id="hidden_field_num_video" value="<?php echo $_POST['$num_video'] ?>" />
           </div>
           <div>
             <label for="video_dur">Duration iof Video?</label>
