440ea26bc1126021b480de0684ce1bf891338038
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-25 14:19:58 +0200

    Update select_data6.py

--- select_data6.py
+++ select_data6.py
@@ -29,21 +29,21 @@ if not (week_day and start_time and num_video and video_delay and video_dur):
     print("Error: Some fields are missing.")
     print("</body></html>")
 else:
-   try:
-      start_time = str(start_time)
-      num_video = int(num_video)
-      video_delay = int(video_delay)
-      video_dur = int(video_dur)
-      execution_string = (
-         "python3 "
-         "regattastart6.py "
-         f"{start_time} {week_day} {video_delay} {num_video} {video_dur} &"
-         )
+    try:
+        start_time = str(start_time)
+        num_video = int(num_video)
+        video_delay = int(video_delay)
+        video_dur = int(video_dur)
+        execution_string = (
+            "python3 "
+            "regattastart6.py "
+            f"{start_time} {week_day} {video_delay} {num_video} {video_dur} &"
+        )
         proc = subprocess.run([execution_string], shell=True)
-   # Process the form data
-   # ...
-   # Send a response back to index6.php
-   response = {"message": "Data processed successfully"}
+        # Process the form data
+        # ...
+        # Send a response back to index6.php
+        response = {"message": "Data processed successfully"}
         print("Content-Type: application/json")
         print()
         print(json.dumps(response))
