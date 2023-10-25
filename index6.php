2854b1e1f3c392497087be7e9301d29db148f9fe
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-25 23:25:53 +0200

    Update index6.php

--- index6.php
+++ index6.php
@@ -74,7 +74,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $execution_string = "python3 /usr/lib/cgi-bin/regattastart6.py $start_time $week_day $video_delay $num_video $video_dur &";
         exec($execution_string);
         // Execute the Python script
-        exec($execution_string, $output, $return_code);
+        
+        //exec($execution_string, $output, $return_code);
         // $output will contain the output of the executed script
         // $return_code will contain the return code of the executed script
 
