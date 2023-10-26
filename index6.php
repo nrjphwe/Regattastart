3f3e672e860033a9384db46ff9d2fb4fa1d069a6
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-26 10:05:46 +0200

    Update index6.php

--- index6.php
+++ index6.php
@@ -57,11 +57,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $video_dur = intval($_POST['video_dur']);
 
         // Build the execution string for your Python script
-        $execution_string = "python3 /usr/lib/cgi-bin/regattastart6.py $start_time $week_day $video_delay $num_video $video_dur &";
-        exec($execution_string);
+        execution_string = "python3 /usr/lib/cgi-bin/regattastart6.py $start_time $week_day $video_delay $num_video $video_dur &";
+
         // Execute the Python script
-        
-        //exec($execution_string, $output, $return_code);
+        //exec($execution_string);
+        exec($execution_string, $output, $return_code);
         // $output will contain the output of the executed script
         // $return_code will contain the return code of the executed script
 
