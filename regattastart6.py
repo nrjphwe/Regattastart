c939e6e385dda294a31723cf45f499d08c0df3cb
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-27 16:37:39 +0200

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -19,7 +19,6 @@ signal_dur = 0.3 # 0.3 sec
 mp4_path = '/var/www/html/images/'
 photo_path = '/var/www/html/images/'
 logger = None  # Declare the logger variable at the top
-logger = setup_logging()
 
 def setup_logging():
     global logger  # Make logger variable global
