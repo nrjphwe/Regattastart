b02e362bca1c1f2bb114dc117ffdd09b716ddee9
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-10-30 12:37:04 +0100

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -182,7 +182,7 @@ def main():
                     #------------------------------------------------------#
                     # video running, duration at "video_dur"
                     #------------------------------------------------------#
-                    logger.info (' dt.datetime.now()= %s, t0= %s, t1= %, t2= %s', dt.datetime.now(),t,t1,t2)  ###
+                    logger.info (' dt.datetime.now()= %s, t0= %s, t1= %, t2= %s',dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),t0.strftime('%Y-%m-%d %H:%M:%S'),t1.strftime('%Y-%m-%d %H:%M:%S'),t2.strftime('%Y-%m-%d %H:%M:%S'))  ###
                     while (dt.datetime.now() - t2).seconds < (60 * video_dur):
                         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "  " + str((dt.datetime.now() - t0).seconds)
                         camera.wait_recording(0.5)
