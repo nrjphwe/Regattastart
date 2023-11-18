715c088dac58d8ba79a22e57768b7b4190aee9e6
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-18 12:46:04 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -65,32 +65,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <?php $day = date("l") 
                     ?>
                     Day for race <select name = "day" id="day">
-                    <option <?php if(isset($day) && $day == "Monday"){echo "selected=\"selected\"";} ?> value="Monday">Monday</option>
-                    <option <?php if(isset($day) && $day == "Tuesday"){echo "selected=\"selected\"";} ?> value="Tuesday">Tuesday</option>
-                    <option <?php if(isset($day) && $day == "Wednesday"){echo "selected=\"selected\"";} ?> value="Wednesday">Wednesday</option>
-                    <option <?php if(isset($day) && $day == "Thursday"){echo "selected=\"selected\"";} ?> value="Thursday">Thursday</option>
-                    <option <?php if(isset($day) && $day == "Friday"){echo "selected=\"selected\"";} ?> value="Friday">Friday</option>
-                    <option <?php if(isset($day) && $day == "Saturday"){echo "selected=\"selected\"";} ?> value="Saturday">Saturday</option>
-                    <option <?php if(isset($day) && $day == "Sunday"){echo "selected=\"selected\"";} ?> value="Sunday">Sunday</option>
+                        <option <?php if(isset($day) && $day == "Monday"){echo "selected=\"selected\"";} ?> value="Monday">Monday</option>
+                        <option <?php if(isset($day) && $day == "Tuesday"){echo "selected=\"selected\"";} ?> value="Tuesday">Tuesday</option>
+                        <option <?php if(isset($day) && $day == "Wednesday"){echo "selected=\"selected\"";} ?> value="Wednesday">Wednesday</option>
+                        <option <?php if(isset($day) && $day == "Thursday"){echo "selected=\"selected\"";} ?> value="Thursday">Thursday</option>
+                        <option <?php if(isset($day) && $day == "Friday"){echo "selected=\"selected\"";} ?> value="Friday">Friday</option>
+                        <option <?php if(isset($day) && $day == "Saturday"){echo "selected=\"selected\"";} ?> value="Saturday">Saturday</option>
+                        <option <?php if(isset($day) && $day == "Sunday"){echo "selected=\"selected\"";} ?> value="Sunday">Sunday</option>
                     </select>
+                    <p></p>
                     <div data-tap-disabled="true">
                     Start Time: <select name = "start_time" id = "start_time">
-                    <?php
-                        $hour = date('H');
-                        $steps   = 10; // only edit the minutes value
-                        $current = 0;
-                        $loops   = 24*(60/$steps);
-                        //$loops   = (24-$hour)*(60/$steps);
-                        //for ($i = $hour*(60/$steps); $i < $loops; $i++) {
-                        for ($i = 0; $i < $loops; $i++) {
-                        //    $time = sprintf('%02d:%02d', $i/(60/$steps), $current%60);
-                        $start_time = sprintf('%02d:%02d', $i/(60/$steps), $current%60);
-                        //echo '<option>' . $start_time . '</option>';
-                        echo '<option value="' . $start_time . '">' . $start_time . '</option>';
-                        $current += $steps;
-                        }
-                    ?>
-                </select>
+                        <?php
+                            $hour = date('H');
+                            $steps   = 10; // only edit the minutes value
+                            $current = 0;
+                            $loops   = 24*(60/$steps);
+                            //$loops   = (24-$hour)*(60/$steps);
+                            //for ($i = $hour*(60/$steps); $i < $loops; $i++) {
+                            for ($i = 0; $i < $loops; $i++) {
+                            //    $time = sprintf('%02d:%02d', $i/(60/$steps), $current%60);
+                            $start_time = sprintf('%02d:%02d', $i/(60/$steps), $current%60);
+                            //echo '<option>' . $start_time . '</option>';
+                            echo '<option value="' . $start_time . '">' . $start_time . '</option>';
+                            $current += $steps;
+                            }
+                        ?>
+                    </select>
             </fieldset>
         </div>
         <div class="w3-half" align="center">
