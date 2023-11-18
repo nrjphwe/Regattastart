3d021a62d75564b1f1c8b4b499e2d9277fadac46
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-18 21:21:02 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -48,7 +48,7 @@ fieldset {
 <div align="center">
 <form action="index6.php" method="POST">
     <!-- Your form fields -->
-    <div class="w3-container w3-sand w3-cell">
+    <div class="w3-container w3-pale-yellow w3-cell">
         <fieldset>
             <legend>Day and time setup: </legend>
             <?php $day = date("l") ?>
@@ -85,7 +85,7 @@ fieldset {
             <br>
         </fieldset>
     </div>
-    <div class="w3-container w3-sand w3-cell">
+    <div class="w3-container w3-pale-yellow w3-cell">
         <fieldset>
             <legend>Video Setup: </legend>
             <p></p>
