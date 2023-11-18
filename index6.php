ae5131f19d9eca2f2c2ed2236101b5b45d00c0da
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-18 18:46:00 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <div align="center">
 <form action="index6.php" method="POST">
     <!-- Your form fields -->
-    <div class="w3-container w3-light-blue w3-cell">
+    <div class="w3-container w3-blue w3-cell">
         <fieldset>
             <legend>Day and time setup: </legend>
             <?php $day = date("l") ?>
