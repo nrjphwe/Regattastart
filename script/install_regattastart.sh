132c9e4b2bcf83b89336dc2d76e588089de420db
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-19 11:16:19 +0100

    Update install_regattastart.sh

--- script/install_regattastart.sh
+++ script/install_regattastart.sh
@@ -8,12 +8,8 @@ cd "$(dirname "$0")/.."
 
 echo "=> Installing regattastart files at CGI-BIN...\n"
 sudo cp -v regattastart7.py /usr/lib/cgi-bin
-sudo cp -v regattastart6.py /usr/lib/cgi-bin
-sudo cp -v select_data7.py /usr/lib/cgi-bin
 sudo cp -v select_data6.py /usr/lib/cgi-bin
 sudo cp -v logging.conf /usr/lib/cgi-bin/
-sudo cp -v dropbox_uploader.sh /usr/lib/cgi-bin/
-sudo cp -vr Dropbox-Uploader /usr/lib/cgi-bin/
 sudo chmod -R 755 /usr/lib/cgi-bin
 sudo chown -R www-data:www-data /usr/lib/cgi-bin
 
@@ -22,9 +18,7 @@ sudo mkdir -v -p /var/www/html/images
 sudo cp -v /var/www/html/index.html /var/www/html/index0.html
 sudo rm -v /var/www/html/index.html
 sudo cp -v index.php /var/www/html
-sudo cp -v index0.php /var/www/html
 sudo cp -v index6.php /var/www/html
-sudo cp -v index7.php /var/www/html
 sudo cp -v w3.css /var/www/html
 sudo chmod -R 755 /var/www/html/
 sudo chown -R www-data:www-data /var/www/html
