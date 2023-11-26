58bdf23e5379e5d896c576b400529b11c198f82c
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-26 21:01:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -166,6 +166,15 @@
     <div class="w3-panel w3-pale-red">
         <?php
             for ($x = 1; $x <= $num_video; $x++) {
+                echo "<h2> Finish video, this is video $x for the finish</h2><br>";
+                $video_name =  "/images/video" . $x . ".mp4";
+                echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
+            }
+        ?>
+
+    
+        <?php
+            for ($x = 1; $x <= $num_video; $x++) {
                 echo "x= $x";
                 $video_name = '/images/video' . $x . '.mp4';
                 if (file_exists($video_name)) {
