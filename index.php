a15c5fd32d97cf31ffbff5b7952b42fab84a6ad1
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-26 19:28:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -153,7 +153,20 @@
             echo "<img id='$filename' src='$imagePath' alt='2a start picture' width='720' height='480'>";
         }
     ?>
+    <div class="w3-panel w3-pale-blue">
+        <?php
+            // Check and display the first video
+            $video_name = '/images/video0.mp4';
+            $imagePath = 'images/' . $videoname; // Relative path
+            if (file_exists($imagePath)) {
+                echo "<h3> Video 5 min före start och 2 min efter, eller vid 2 starter, till 2 min efter andra start </h3>";
+                echo '<video width = "720" height="480" controls><source src= ' . $video_name . ' type="video/mp4"></video><p>';
+            }
+        ?>
     </div>
+
+
+
     <div class="w3-panel w3-pale-blue">
         <?php
             $video_name = '/images/video0.mp4';
