30d58d55dec95c1fcc1dce17759057f1b4f16a73
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-11-30 14:29:35 +0100

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -217,14 +217,12 @@ def main():
                         while (dt.datetime.now() - t0).seconds < (119):
                             now = dt.datetime.now()
                             seconds_since_midnight = now.hour * 3600 + now.minute * 60 + now.second
-                            logger.info("Inside inner loop. seconds_since_midnight=%s", seconds_since_midnight)
+                            #logger.info("Inside inner loop. seconds_since_midnight=%s", seconds_since_midnight)
                             annotate_video_duration(camera, start_time_sec)
-                            #camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "  Seconds since last start: " + str((dt.datetime.now() - t0).seconds)
                             camera.wait_recording(0)
                     
                         stop_video_recording(camera)
                         convert_video_to_mp4(mp4_path, "video0.h264", "video0.mp4")
-
                     # Exit the loop after the condition is met
                     break
 
