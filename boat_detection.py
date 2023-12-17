2642b45e91253bae8401a296c5212331190970c3
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-17 13:42:43 +0100

    Update boat_detection.py

--- boat_detection.py
+++ boat_detection.py
@@ -1,7 +1,8 @@
-boat_detection.py
+#!/usr/bin/python3 
 import cv2
 
 # Load the pre-trained object detection model
+# YOLO (You Only Look Once) 
 net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
 
 # Load COCO names (class labels)
@@ -16,8 +17,12 @@ cap = cv2.VideoCapture(0)  # 0 for default camera
 
 while True:
     ret, frame = cap.read()
+    # ret is a boolean indicating whether the frame was successfully 
+    # captured and frame is the captured frame
 
-    # Perform object detection
+    # Perform object detection, preprocess the frame for object 
+    # detection using YOLO. The frame is converted into a blob, and
+    # the YOLO model is fed with this blob to obtain the detection results.
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(layer_names)
@@ -32,10 +37,10 @@ while True:
             if confidence > 0.5 and classes[class_id] == 'boat':
                 # Implement your logic to start/stop recording here
                 print("Boat detected!")
-
+    # Display the frame with the detection results.
     cv2.imshow('Boat Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+# After loop, the script release camera and closes the OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
