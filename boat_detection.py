b80fdb1e9ebb09f47ba8798e8010d3f34a46d5f3
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-19 15:18:44 +0100

    Update boat_detection.py

--- boat_detection.py
+++ boat_detection.py
@@ -26,7 +26,7 @@ layer_names = net.getUnconnectedOutLayersNames()
 cap = cv2.VideoCapture(0)  # 0 for default camera
 
 # Set the frame skipping factor, (to reduce latency)
-frame_skip_factor = 25
+frame_skip_factor = 15
 frame_counter = 0
 
 while True:
