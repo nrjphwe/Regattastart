d8a786d809081dd9984b5d72216dc52019d2d9c7
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-22 14:19:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -68,6 +68,17 @@ while True:
                     # Reset the timer and stop capturing frames
                     start_time = 0
                     boat_detected = False
+            
+            # Check if the capture duration has passed
+            elapsed_time = time.time() - start_time
+            if elapsed_time < capture_duration:
+                # Write the frame to the video file during the capture duration
+                video_writer.write(frame)
+            else:
+                # Reset the timer and stop capturing frames
+                start_time = 0
+                boat_detected = False
+
 
     # Display the frame in the 'Video' window
     cv2.imshow("Video", frame)
