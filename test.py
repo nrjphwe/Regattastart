68d52bfa5e084feeea390a03d4037e29612c6379
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-22 19:50:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
         
-            if confidence > 0.2 and classes[class_id] == 'boat':
+            if confidence > 0.3 and classes[class_id] == 'boat':
                 boat_detected = True
                 print(time.strftime("%Y-%m-%d-%H:%M:%S"), f"Class: {classes[class_id]}, Confidence: {confidence}")
                 # Visualize the detected bounding box
