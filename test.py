ea2371c5567d1e7b5c9e2f048eae090ff17bffa7
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-23 11:37:12 +0100

    Update test.py

--- test.py
+++ test.py
@@ -59,12 +59,16 @@ while True:
                 # Modify the original frame
                 cv2.rectangle(frame, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2, cv2.LINE_AA)
                 
-                # confidence > 0.3 
+                # confidence > 0.2 
                 print(time.strftime("%Y-%m-%d-%H:%M:%S"),"write frame > 0.3")
                 # Write frames to the video file
-                video_writer.write(frame)
+                i = 1
+                    while i < number_of_frames:
+                        # Write frames to the video file
+                        video_writer.write(frame)
+                        i += 1
             else:
-                # Confidence < 0.3
+                # Confidence < 0.2
                 if boat_detected == True:
                     i = 1
                     while i < number_of_frames:
