04353ad5492fe8f0d1bbee9319c4cfa7462a06f3
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-24 23:10:09 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -156,7 +156,7 @@ def cv_annotate_video(frame, start_time_sec):
 
 def finish_recording(mp4_path, video_dur, start_time_sec):
     # Load the pre-trained object detection model -- YOLO (You Only Look Once) 
-    net = cv2.dnn.readNet('../darknet/yolov3-tiny.weights', '../darknet/cfg/yolov3-tiny.cfg')
+    net = cv2.dnn.readNet('/home/pi/darknet/yolov3-tiny.weights', '/home/pi/darknet/cfg/yolov3-tiny.cfg')
 
     # Load COCO names (class labels)
     with open('../darknet/data/coco.names', 'r') as f:
