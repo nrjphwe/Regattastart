755dceee0e8b1d2fe3897b4d4e7ae81d0dd9b75f
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-25 15:50:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -56,11 +56,12 @@
         echo ", Form data " . $form_data; 
 
     if (isset($_SESSION["num_starts"]))
+        $num_starts = $_SESSION['form_data'];
         $num_starts= $form_data['num_starts'];
         echo ", Number of starts: " . $num_starts; 
 
     if (isset($_SESSION["video_end"]))
-        //$video_end = $_SESSION['form_data'];
+        $video_end = $_SESSION['form_data'];
         $video_end = $form_data['video_end'];
         echo ", Max duration from start: " . $video_end; 
 
