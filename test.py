3cd90ad8d10f37ba658e5faf31281316bf079ce4
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2023-12-29 19:49:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -93,8 +93,8 @@ while True:
                             i += 1
                     boat_detected = False
                     
-            # Display the frame in the 'Video' window
-            cv2.imshow("Video", frame)
+                # Display the frame in the 'Video' window
+                cv2.imshow("Video", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
