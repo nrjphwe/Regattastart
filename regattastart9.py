dec6efd34466bf2268dd51b1e7fc2b73e2b12998
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-01-02 20:29:42 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -247,9 +247,10 @@ def finish_recording(mp4_path, num_starts, video_end, start_time, start_time_sec
                     while i < number_of_non_detected_frames:
                         # Record new frames for additional_seconds after the last boat detection
                         ret, frame = cap.read()  # Read new frames
-                        if not ret or frame is None:
-                            print("End of video stream or frame is None")
+                        if frame is None or not ret:
+                            print("End of video stream or frame is None.")
                             break
+
                         cv_annotate_video(frame, start_time_sec)
                         video_writer.write(frame)
                         i += 1
