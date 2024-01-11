e6cf07f5f93bdecabbf008405c121c671c37cae8
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-01-11 16:09:42 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -212,8 +212,8 @@ def finish_recording(mp4_path, num_starts, video_end, start_time, start_time_sec
     cap = open_camera()
 
     # Initialize variables
-    number_of_detected_frames = 5 # Set the number of frames to record after detecting a boat
-    number_of_non_detected_frames = 25
+    number_of_detected_frames = 10 # Set the number of frames to record after detecting a boat
+    number_of_non_detected_frames = 5
     start_time = time.time()  # Record the start time of the recording
     fps = 24  # frames per second
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
@@ -356,7 +356,6 @@ def main():
             signal.close() # Turn off the signal output
             lamp1.close() # Turn off the lamp1
             lamp2.close() # Turn off the lamp2
-        #GPIO.cleanup()
 
 if __name__ == "__main__":
     #logging.basicConfig(level=logging.WARNING)  # Set log level to WARNING
