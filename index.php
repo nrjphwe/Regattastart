2902934fb41e41a23feb224ac01ecd818de8d22b
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-07 17:04:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     // after "git pull", "sudo cp /home/pi/Regattastart/index.php /var/www/html/"
     ini_set('display_errors', 1); 
     error_reporting(E_ALL);
+    print_r($_SESSION);
+    echo "<br/>";
+    print_r($_POST);
+    echo "<br/>";
 ?>
 <?php
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
