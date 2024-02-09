0bcc2d06dd6a7b37a81f16e43e423580272be69f
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-09 11:29:18 +0100

    Update boat_detection.py

--- boat_detection.py
+++ boat_detection.py
@@ -42,7 +42,7 @@ frame_size = (640, 480)
 # video_writer = None
 # Open a video capture object 0 for webcam)
 cap = cv2.VideoCapture(0)
-out = video_writer = cv2.VideoWriter('output'+ today + '.mp4', fourcc, fps_out, frame_size)
+out = video_writer = cv2.VideoWriter('output'+ today + '.x2644', fourcc, fps_out, frame_size)
 fps = 24  # frames per second
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
