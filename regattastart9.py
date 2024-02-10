1b98df5c507cf4c57ddf68d76b1accc5fc8a04da
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-10 14:49:08 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -255,7 +255,7 @@ def finish_recording(video_path, num_starts, video_end, start_time, start_time_s
     # setup cv2 writer 
     #fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # H.264 codec with MP4 container
     fourcc = cv2.VideoWriter_fourcc(*'x264')  # H.264 codec with MP4 container
-    video_writer = cv2.VideoWriter(video_path + 'video1' + '.h2644', fourcc, fpsw, frame_size)
+    video_writer = cv2.VideoWriter(video_path + 'video1' + '.h264', fourcc, fpsw, frame_size)
 
     number_of_non_detected_frames = 30
     number_of_detected_frames = 3 # Set the number of frames to record after detecting a boat
