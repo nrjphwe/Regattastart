11f6af6ff9e8f2a6ff34e06ab18641ecd197a41d
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-12 15:22:09 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -180,10 +180,10 @@ def cv_annotate_video(frame, start_time_sec):
     elapsed_time = seconds_since_midnight - start_time_sec #elapsed since last start until now)
     label = str(dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) +  " Seconds since last start: " +  str(elapsed_time)
     org = (30,60)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font = ImageFont.truetype("PAPYRUS.ttf", 80) 
+    #font = cv2.FONT_HERSHEY_SIMPLEX
+    #font = ImageFont.truetype("PAPYRUS.ttf", 80) 
     fontFace=cv2.FONT_HERSHEY_DUPLEX
-    fontScale = 0.6
+    fontScale = 0.7
     color=(0,0,255) #(B, G, R)
     thickness = 1
     lineType = cv2.LINE_AA
