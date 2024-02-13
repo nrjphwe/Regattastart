fb6e6a9701c38e63856a98304c66243f73647a17
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-13 22:43:48 +0100

    Update boat_detection.py

--- boat_detection.py
+++ boat_detection.py
@@ -47,100 +47,88 @@ fps = 24  # frames per second
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
 frame_size = (width, height)
-
-
-# Initialize PiCamera outside the loop
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    time.sleep(2)  # Allow the camera to warm up
     
-    while True:
+while True:
 
-        ret, frame = cap.read()
-        if not ret:
-            break
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-        # Open the PiCamera as a stream and convert it to a numpy array
-        stream = picamera.array.PiRGBArray(camera, size=frame_size )
-        print(today, "Before frame capture,line 55")
-        camera.capture(stream, format='bgr')
-        print(today, "After frame capture, line 57")
 
-        frame = np.copy(stream.array)
 
-        # Increment the frame counter
-        frame_counter += 1
+    # Increment the frame counter
+    frame_counter += 1
 
-        # Only process frames that meet the skipping criteria
-        if frame_counter % frame_skip_factor == 0:
-            frame_counter = 0
+    # Only process frames that meet the skipping criteria
+    if frame_counter % frame_skip_factor == 0:
+        frame_counter = 0
 
-            # Perform object detection, preprocess the frame for object 
-            # detection using YOLO. The frame is converted into a blob, and
-            # the YOLO model is fed with this blob to obtain the detection results.
-            #blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-            today = time.strftime("%Y%m%d-%H%M%S")
-            print(today, "Before object detection, line 70")
-            blob = cv2.dnn.blobFromImage(frame, scalefactor=0.00392, size=(416, 416), swapRB=True, crop=False)
-            net.setInput(blob)
-            outs = net.forward(layer_names)
+        # Perform object detection, preprocess the frame for object 
+        # detection using YOLO. The frame is converted into a blob, and
+        # the YOLO model is fed with this blob to obtain the detection results.
+        #blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        today = time.strftime("%Y%m%d-%H%M%S")
+        print(today, "Before object detection, line 70")
+        blob = cv2.dnn.blobFromImage(frame, scalefactor=0.00392, size=(416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(layer_names)
 
-            print(today, "After object detection")
+        print(today, "After object detection")
 
-            # Variable to check if any boat is detected in the current frame
-            boat_detected = False
+        # Variable to check if any boat is detected in the current frame
+        boat_detected = False
 
-            # Process the detection results
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    class_id = np.argmax(scores)
-                    confidence = scores[class_id]
-                
-                    if confidence > 0.2 and classes[class_id] == 'boat':
-                        # Update the time when the last boat was detected
-                        last_detection_time = time.time()
-                        today = time.strftime("%Y%m%d-%H%M%S")
-                        print(today, f"Class: {classes[class_id]}, Confidence: {confidence}")
-                        # Visualize the detected bounding box
-                        # Visualize the detected bounding box
-                        h, w, _ = frame.shape
-                        x, y, w, h = map(int, detection[0:4] * [w, h, w, h])
-                        pt1 = (int(x), int(y))
-                        pt2 = (int(x + w), int(y + h))
-                        # Modify the original frame
-                        cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 2, cv2.LINE_AA)
+        # Process the detection results
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+            
+                if confidence > 0.2 and classes[class_id] == 'boat':
+                    # Update the time when the last boat was detected
+                    last_detection_time = time.time()
+                    today = time.strftime("%Y%m%d-%H%M%S")
+                    print(today, f"Class: {classes[class_id]}, Confidence: {confidence}")
+                    # Visualize the detected bounding box
+                    # Visualize the detected bounding box
+                    h, w, _ = frame.shape
+                    x, y, w, h = map(int, detection[0:4] * [w, h, w, h])
+                    pt1 = (int(x), int(y))
+                    pt2 = (int(x + w), int(y + h))
+                    # Modify the original frame
+                    cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 2, cv2.LINE_AA)
 
-                        label = "text"
-                        org = (30,60)
-                        #font = cv2.FONT_HERSHEY_SIMPLEX
-                        #font = ImageFont.truetype("PAPYRUS.ttf", 80) 
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX
-                        fontScale = 0.7
-                        color=(0,0,255) #(B, G, R)
-                        thickness = 1
-                        lineType = cv2.LINE_AA
-                        cv2.putText(frame,label,org,fontFace,fontScale,color,thickness,lineType)
+                    label = "text"
+                    org = (30,60)
+                    #font = cv2.FONT_HERSHEY_SIMPLEX
+                    #font = ImageFont.truetype("PAPYRUS.ttf", 80) 
+                    fontFace=cv2.FONT_HERSHEY_DUPLEX
+                    fontScale = 0.7
+                    color=(0,0,255) #(B, G, R)
+                    thickness = 1
+                    lineType = cv2.LINE_AA
+                    cv2.putText(frame,label,org,fontFace,fontScale,color,thickness,lineType)
 
-                        # Trigger video recording
-                        if not recording:
-                            recording = True
-                            out.write(frame)
-                            print(today, "Recording started, line 110")
-                            boat_detected = True
-                    else:
-                        boat_detected = False
+                    # Trigger video recording
+                    if not recording:
+                        recording = True
+                        out.write(frame)
+                        print(today, "Recording started, line 110")
+                        boat_detected = True
+                else:
+                    boat_detected = False
 
-            # Display the frame with the detection results.
-            cv2.imshow('Boat Detection', frame)
+        # Display the frame with the detection results.
+        cv2.imshow('Boat Detection', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-    #  Pressing 'q' will exit the script.
-    # After loop, the script release camera and closes the OpenCV windows
-    if camera is not None:
-        camera.close()  # Release the camera resources
-    if video_writer is not None:
-        video_writer.release()
-    cv2.destroyAllWindows()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+#  Pressing 'q' will exit the script.
+# After loop, the script release camera and closes the OpenCV windows
+if camera is not None:
+    camera.close()  # Release the camera resources
+if video_writer is not None:
+    video_writer.release()
+cv2.destroyAllWindows()
