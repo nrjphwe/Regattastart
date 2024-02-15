fd53e125a1ff1c79b8c1ab1b127905267c189f0a
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-15 13:18:28 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -59,6 +59,10 @@ $num_starts = isset($_SESSION["form_data"]["num_starts"]) ? $_SESSION["form_data
             height: 100vh; /* Optional: Makes the container full height */
         }
         .form-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Adjust as needed */
             width: 80%; /* Adjust width as needed */
             margin: 0 auto; /* Center horizontally */
         }
@@ -70,7 +74,6 @@ $num_starts = isset($_SESSION["form_data"]["num_starts"]) ? $_SESSION["form_data
             padding: 10px;
             border-radius: 5px;
             border: 1px solid #ccc;
-            margin: 0 auto; /* Center horizontally */
             margin-bottom: 10px;
         }
         .form-container button[type="submit"] {
