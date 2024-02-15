ce64dd8ea14bba15874d73bc627f48aa2b770542
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-15 21:39:54 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -214,16 +214,15 @@ $num_starts = isset($_SESSION["form_data"]["num_starts"]) ? $_SESSION["form_data
     <div class="w3-row-padding" align="center">
         <br><p> - phwe - <br></p>
     </div>
-    <div class="w3-row-padding" align="center">
+    <div style="text-align: center;" class="w3-panel w3-grey">
         <?php
             // output when index6.php was last modified.
             $filename = 'index6.php';
             if (file_exists($filename)) {
-                echo "        This web-page: $filename was last modified: " . date ("Y-m-d H:i:s.", filemtime($filename));
+                echo "This web-page: $filename was last modified: " . date ("Y-m-d H:i:s.", filemtime($filename));
             }
         ?>
     </div>
 </footer>
 </body>
 </html>
-
