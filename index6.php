8b2053a05baf03907ca77afa7086e7a67c7f8fd1
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-15 18:34:49 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -66,7 +66,7 @@ $num_starts = isset($_SESSION["form_data"]["num_starts"]) ? $_SESSION["form_data
 <!-- Content Wrapper with center alignment -->
 <div class="content-wrapper">
 <!-- HTML form -->
-<div class="form-container" style="text-align: center;">
+<div class="w3-container" style="text-align: center;">
     <form action="index6.php" method="POST">
         <div class="w3-row-padding">
             <div class="w3-cell">
