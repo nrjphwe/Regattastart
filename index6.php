f52f1da679452b8a836e2b6744222a6844c63c7d
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-15 17:17:06 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -212,7 +212,7 @@ $num_starts = isset($_SESSION["form_data"]["num_starts"]) ? $_SESSION["form_data
 
 <!-- Here is our page's main content -->
 <main>
-    <div class="w3-row-padding" style="text-align: center;">
+    <div class="w3-auto w3-row-padding" align="center" style="text-align: center;">
         <div id="result" ></div>
     </div>
     <h5><a href="/index.php">  Resultat sida  </a></h5>
