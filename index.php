b02924bfc68258c1c96c7ea7c44961dabf645267
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-16 17:23:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,20 @@
     error_reporting(E_ALL);
 ?>
 <?php
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if ($_SERVER["REQUEST_METHOD"] === "POST")
+    {
         // Form was submitted
         include_once "stop_recording.php"; // Include the script to stop recording
-        error_log('Line 13: The stop_recording.php was included in index.php')
+        error_log('Line 13: The stop_recording.php was included in index.php');
 
-        // Trigger a refresh of the page after a certain time interval (e.g., 20 seconds)
-        echo '<meta http-equiv="refresh" content="20">';
-    } elseif ($_SERVER["REQUEST_METHOD"] !== "GET") {
+        // Trigger a refresh of the page after a certain time interval e.g., 10
+
+        echo "<meta http-equiv="refresh" content="10" >";
+
+    } elseif ($_SERVER["REQUEST_METHOD"] !== "GET")
+    {
         // Log an error only if the request method is neither "POST" nor "GET"
-        error_log('Line 19: $_SERVER["REQUEST_METHOD"] < > "POST" NOR "GET" ');
+        error_log('Line 17: $_SERVER["REQUEST_METHOD"] < > "POST" NOR "GET" ');
     }
 ?>
 <!-- Your HTML to display data from the session -->
