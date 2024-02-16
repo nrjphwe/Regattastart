55db8198390283eff945c3b72ffae201d997859e
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-16 12:05:08 +0100

    Update index9.php

--- index9.php
+++ index9.php
@@ -172,11 +172,11 @@
                             <p></p>
                             End for finish video, duration from last start:
                             <select name = "video_end" id = "video_end">
-                                <option value="10" <?php if(isset($video_end) && $video_end == "5"){echo "selected=\"selected\"";} ?> value="10">10</option>
-                                <option value="20" <?php if(isset($video_end) && $video_end == "20"){echo "selected=\"selected\"";} ?> value="20">20</option>
                                 <option value="60" <?php if(isset($video_end) && $video_end == "60"){echo "selected=\"selected\"";} ?> value="60">60</option>
-                                <option value="90" <?php if(isset($video_end) && $video_end == "90"){echo "selected=\"selected\"";}?> value="90">90</option>
+                                <option value="90" <?php if(isset($video_end) && $video_end == "90"){echo "selected=\"selected\"";} ?> value="90">90</option>
                                 <option value="120" <?php if(isset($video_end) && $video_end == "120"){echo "selected=\"selected\"";} ?> value="120">120</option>
+                                <option value="180" <?php if(isset($video_end) && $video_end == "180"){echo "selected=\"selected\"";} ?> value="180">120</option>
+                                <option value="10" <?php if(isset($video_end) && $video_end == "10"){echo "selected=\"selected\"";} ?> value="10">10</option>
                             </select>
                             <p></p>
                         </fieldset>
