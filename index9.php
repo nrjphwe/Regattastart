4a4564860f7b9fedf46a4dc3929b74eb9c15f7aa
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-23 19:06:21 +0100

    Update index9.php

--- index9.php
+++ index9.php
@@ -156,11 +156,10 @@
                             Start Time: <select name="start_time" id="start_time">
                                 <?php
                                     for ($i = 0; $i < $loops; $i++) {
-                                        //$start_time_option = date('H:i', $current);
                                         $start_time_option = sprintf('%02d:%02d', $i / (60 / $steps), $current % 60);
                                         $selected = ($start_time == $start_time_option) ? "selected" : ""; // Check if this option should be selected
                                         echo '<option value="' . $start_time_option . '" ' . $selected . '>' . $start_time_option . '</option>';
-                                        $current += $steps * 60; // Increment by $steps in minutes
+                                        $current += $steps;
                                     }
                                 ?>
                             </select>
