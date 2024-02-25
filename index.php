0f5562948c56e988a49e2f4faecf991b2ca78eb3
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-25 10:50:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -98,12 +98,12 @@
                 $second_start_minute = $second_start_time_minutes % 60;
                 // Format second start time
                 $second_start_time = sprintf('%02d:%02d', $second_start_hour, $second_start_minute);
-                echo ", 2nd Start is at: $second_start_time";
+                echo ", 2nd Start at: $second_start_time";
             }
         }
         if (isset($video_dur)) {
             echo "<br>";
-            echo ", Video duration: $video_dur min,"  ;
+            echo " Video duration: $video_dur min,"  ;
             echo " Video delay after start: $video_delay min,";
             echo " Number of videos during finish: " . $num_video;
         }
