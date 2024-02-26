2ba5831e187defc51770127721e66518487b99cb
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-26 09:48:14 +0100

    Update index9.php

--- index9.php
+++ index9.php
@@ -157,7 +157,7 @@
                             Start Time: <select name="start_time" id="start_time">
                                 <?php
                                     for ($i = 0; $i < $loops; $i++) {
-                                        $start_time_option = sprintf('%02d:%02d', $i / (60 / $steps), $current % 60);
+                                        $start_time_option = sprintf('%02d:%02d', $i / (60 / $steps), $nearest_time % 60);
                                         //$selected = ($start_time == $start_time_option) ? "selected" : ""; // Check if this option should be selected
                                         $selected = ($start_time == $start_time_option || $start_time_option == $nearest_time_str) ? "selected" : ""; // Check if this option should be selected
                                         echo '<option value="' . $start_time_option . '" ' . $selected . '>' . $start_time_option . '</option>';
