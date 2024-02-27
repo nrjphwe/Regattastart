92e5f9f0477719bd9b03fdcf2dcecfbbbfa1a66d
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-27 15:48:06 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -104,7 +104,9 @@
                                 $start_time = isset($_SESSION["form_data"]["start_time"]) ? $_SESSION["form_data"]["start_time"] : "";
                                 $steps = 5; // Set to 10, for test set to 5, You can adjust the value of $steps according to your needs
                                 $loops = 24 * (60 / $steps); // Define $loops here or wherever it makes sense in your code
-                                $current = strtotime('today'); // Get the current timestamp truncated to the beginning of the day
+
+                                //$current = strtotime('today'); // Get the current timestamp truncated to the beginning of the day
+                                $current = time(); 
                                 echo "Line 108: current = ", date("l H:i:s", $current), "<p>";
 
                                 $seconds_since_midnight = $current - strtotime('today');
