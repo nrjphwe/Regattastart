3341e9241e0082e1dbbb92754341acac44e86027
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-27 15:35:30 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -105,7 +105,8 @@
                                 $steps = 5; // Set to 10, for test set to 5, You can adjust the value of $steps according to your needs
                                 $loops = 24 * (60 / $steps); // Define $loops here or wherever it makes sense in your code
                                 $current = strtotime('today'); // Get the current timestamp truncated to the beginning of the day
-                                $nearest_time = ceil((time() - $current) / 300) * 300; // Find the nearest time in 5-minute intervals
+                                $nearest_time = strtotime('today') + round($seconds_since_midnight / (5 * 60)) * (5 * 60);
+                                //$nearest_time = ceil((time() - $current) / 300) * 300; // Find the nearest time in 5-minute intervals
                                 $start_time_option = date('H:i', $nearest_time);
                             ?>
                             Start Time: <select name="start_time" id="start_time">
