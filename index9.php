98043fb2ef176746c36864e3ac3baa72b2b60c93
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-27 15:32:17 +0100

    Update index9.php

--- index9.php
+++ index9.php
@@ -174,7 +174,8 @@
                                 // Loop through the intervals in a day starting from the nearest time
                                 for ($i = 0; $i < $loops; $i++) {
                                     /// Convert the current time to a formatted string
-                                    $time_option = sprintf('%02d:%02d', $i / (60 / $steps), $current % 60);
+                                    $time_option = date('H:i', $current);
+                                    //$time_option = sprintf('%02d:%02d', $i / (60 / $steps), $current % 60);
                                     // Check if this option should be selected
                                     $selected = ($time_option == $start_time_option) ? "selected" : ""; // Check if this option should be selected
                                     echo '<option value="' . $time_option . '" ' . $selected . '>' . $time_option . '</option>';
