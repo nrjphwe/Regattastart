784dba0e7d61bda9590668749315b6253a246886
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-02-27 15:14:37 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -111,10 +111,11 @@
                             Start Time: <select name="start_time" id="start_time">
                                 <?php
                                     for ($i = 0; $i < $loops; $i++) {
+                                        // Convert the current time to a formatted string
                                         $time_option = sprintf('%02d:%02d', $i / (60 / $steps), $current % 60);
+                                        // Check if this option should be selected
                                         $selected = ($start_time == $time_option) ? "selected" : ""; // Check if this option should be selected
-                                        echo '<option value="' . $start_time_option . '" ' . $selected . '>' . $start_time_option . '</option>';
-                                        $current += $steps;
+                                        echo '<option value="' . $time_option . '" ' . $selected . '>' . $time_option . '</option>';
                                     }
                                 ?>
                             </select>
