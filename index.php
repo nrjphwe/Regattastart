7d753166354b96cd15c5e2fbe58af9eadce4483a
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-03-02 00:07:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -59,6 +59,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="30">
     <link rel="stylesheet" href="/w3.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- <meta http-equiv="refresh" content="200" -->
