a911fae0510a6b72fc606fd44ad0bf36015b6ff1
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-03-06 22:28:57 +0100

    Create check_Video_Completion.php

---
+++ check_Video_Completion.php
@@ -0,0 +1,12 @@
+<scriot>
+function isVideo1Completed() {
+    // Read the content of the status file
+    $status = trim(file_get_contents('/var/www/html/status.txt'));
+    error_log("Line 39: isVideo1Completed status= " . $status);
+    // Check if the status indicates Video1 completion
+    return ($status === 'complete');
+}
+
+// Check Video1 completion status
+$video1Completed = isVideo1Completed();
+</script>
