1b30dc83d5f0ec94f11564e0a38a5a8d6ab486d7
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-03-06 22:41:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -397,11 +397,7 @@
     </div>
     <!--- Java Scripts  -->
     <script> // Function to check Video1 completion status and reload page if complete
-        //var checkCompletionInterval;
-        //var video1Exists = <?php echo json_encode(xvideo1Exists); ?>; // Get the value from PHP
-        //var video1Completed = <?php echo json_encode(xvideo1Completed); ?>; // Get the value from PHP
-        //console.log('Line 412: xxxxx Video1 completion check response:', video1Completed);
-
+        ß
         function checkVideoCompletion() {
             // AJAX call to PHP script to check completion status
             $.ajax({
