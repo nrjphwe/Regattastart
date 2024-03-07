49e6a40503ad7dca38a9acbe04f30c1783f778cf
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-03-07 13:13:05 +0100

    Update check_video_completion.php

--- check_video_completion.php
+++ check_video_completion.php
@@ -4,7 +4,8 @@ function isVideo1Completed() {
     $status = trim(file_get_contents('/var/www/html/status.txt'));
     error_log("check_video_completion, Line 39: isVideo1Completed status= " . $status);
     // Check if the status indicates Video1 completion
-    return ($status === 'complete');
+    //return ($status === 'complete');
+    return ($status);
 }
 // Check Video1 completion status
 $video1Completed = isVideo1Completed();
