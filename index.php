f0c4c7f6b5238e76af480ff3d7e004d3dd375185
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-03-07 11:41:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -404,25 +404,20 @@
    
         function checkVideoCompletion() {
             // AJAX call to PHP script to check completion status
-            $.ajax({
-                url: 'check_video_completion.php',
-                type: 'GET',
-                success: function(response) {
-                    console.log('Line 411: function response=',response);
+            $.get('check_video_completion.php', function(response, status) {
+                alert("Data: " + response + "\nStatus: " + status);
+                    console.log('Line 409: function response=',response);
                     var trimmed_response = response.trim(); // Trim the response text
-                    console.log('Line 413: Video1 completion check response:', trimmed_response);
+                    console.log('Line 411: Video1 completion check response:', trimmed_response);
                     // If Video1 is completed, reload the page
                     if (trimmed_response === 'true') {
-                        console.log('Line 416: Reloading page...');
+                        console.log('Line 414: Reloading page...');
                         location.reload(true); // Reload with hard refresh
                     } else {
-                        console.log('Line 419: Video1 not completed yet.');
+                        console.log('Line 417: Video1 not completed yet.');
                         //location.reload(); // Reload
                     }
-                },
-                error: function(xhr, status, error) {
-                    console.error('Line 433: Error checking video completion:', error);
-                } 
+            
             });
         }
         if (video1Exists) 
