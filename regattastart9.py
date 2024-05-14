b5b92bc50205f24274bb637ec6e7b3b8d4b6c620
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-05-14 22:27:11 +0200

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -151,14 +151,14 @@ def start_sequence(camera, start_time_sec, num_starts, dur_between_starts, photo
 
         # Define time intervals for each relay trigger
         time_intervals = [
-            (start_time_sec - 5 * (60 - 1,5), lambda: trigger_relay('Lamp1_on'), "5_min Lamp-1 On -- Up with Flag O"),
+            (start_time_sec - 5 * 60 - 2, lambda: trigger_relay('Lamp1_on'), "5_min Lamp-1 On -- Up with Flag O"),
             (start_time_sec - 5 * 60, lambda: trigger_relay('Signal'), "5_min Warning signal"),
-            (start_time_sec - 4 * (60 - 1,5), lambda: trigger_relay('Lamp2_on'), "4_min Lamp-2 On"),
+            (start_time_sec - 4 * 60 - 2, lambda: trigger_relay('Lamp2_on'), "4_min Lamp-2 On"),
             (start_time_sec - 4 * 60, lambda: trigger_relay('Signal'), "4_min Warning signal"),
-            (start_time_sec - 1 * (60 - 1,5), lambda: trigger_relay('Lamp2_off'), "1_min Lamp-2 off -- Flag P down"),
+            (start_time_sec - 1 * 60 - 2, lambda: trigger_relay('Lamp2_off'), "1_min Lamp-2 off -- Flag P down"),
             (start_time_sec - 1 * 60, lambda: trigger_relay('Signal'), "1_min  Warning signal"),
-            (start_time_sec - 1, lambda: trigger_relay('Lamp1_off'), "Lamp1-off at start"),
-            (start_time_sec, lambda: trigger_relay('Signal'), "Start signal"),
+            (start_time_sec - 0 * 60 -2, lambda: trigger_relay('Lamp1_off'), "Lamp1-off at start"),
+            (start_time_sec - 0 * 60, lambda: trigger_relay('Signal'), "Start signal"),
         ]
 
         last_triggered_events = {}
