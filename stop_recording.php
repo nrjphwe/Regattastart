82241ff9049acb3cb84c1469b0f8251b9279b375
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-12-17 23:57:15 +0100

    Update stop_recording.php

--- stop_recording.php
+++ stop_recording.php
@@ -1,4 +1,13 @@
 <?php
+    function console_log($output, $with_script_tags = true) {
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+        echo $js_code;
+    }
+?>
+<?php
 // Specify the correct path to the named pipe
 $pipePath = '/var/www/html/tmp/stop_recording_pipe';
 
@@ -7,25 +16,25 @@ $pipeHandle = fopen($pipePath, 'w');
 if ($pipeHandle === false) {
     // Handle error
     $lastError = error_get_last();
-    error_log('Failed to open named pipe: ' . $lastError['message']);
+    console_log('Stop_recording.php Failed to open named pipe: ' . $lastError['message']);
     die('Failed to open named pipe.');
 } else {
-    error_log('Line 13: Opened pipe handle successfully: ' . $pipeHandle);
+    console_log('Stop_recording.php Line 22: Opened pipe handle successfully: ' . $pipeHandle);
 }
 
 $message = 'stop_recording';
 if (fwrite($pipeHandle, $message) === false) {
     // Log error if writing to pipe fails
-    error_log('Line 19: Failed to write message to named pipe.');
+    console_log('Stop_recording.php Line 28: Failed to write message to named pipe.');
 } else {
     // Log success message
-    error_log('Line 22: Message sent to named pipe: ' . $message);
+    console_log('Stop_recording.php Line 31: Message sent to named pipe: ' . $message);
 }
 
 if (fclose($pipeHandle) === false) {
-    error_log('Line 26: Failed to close pipe.');
+    console_log('Stop_recording.php Line 38: Failed to close pipe.');
 } else {
     // Log success message
-    error_log('Line 29: succesfully closed the pipe:');
+    console_log('Stop_recording.php Line 38: succesfully closed the pipe:');
 }
 ?>
