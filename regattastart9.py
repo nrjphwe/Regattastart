e55cd3f61e2c1c9e3c5cb3d6c53156dcf74fa408
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-01-15 22:58:25 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -515,7 +515,7 @@ def finish_recording(cam, video_path, num_starts, video_end, start_time):
             confidence = row['confidence']
             x1, y1, x2, y2 = int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])
 
-        logger.debug(f"Confidence: {confidence}, Class Name: {class_name}")
+        # logger.debug(f"Confidence: {confidence}, Class Name: {class_name}")
         if confidence > 0.2 and class_name == 'boat':  # Check if detection is a boat
             boat_in_current_frame = True
             logger.info("Boat detected, saving pre-detection frames.")
