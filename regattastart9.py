dd375502c6e333aa3911902d7068ca10f3419b90
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-01-15 19:12:28 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -621,7 +621,7 @@ def main():
                         logger.info(f"(dt.datetime.now() - t0).seconds: {(dt.datetime.now() - t0).seconds}")
                         while (dt.datetime.now() - t0).seconds < (119):
                             now = dt.datetime.now()
-                            logger.info(f"now {now}")
+                            logger.info(f"now {now} strftime: {time.strftime("%H:%M:%S",now)} ")
                             logger.info(f"seconds_since_midnight {seconds_since_midnight}")
                             seconds_since_midnight = now.hour * 3600 + now.minute * 60 + now.second
                             logger.info(f"dt.datetime.now(): {dt.datetime.now()} t0: {t0}")  # test
