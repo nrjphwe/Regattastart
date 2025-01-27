654d16ffea1313105aaa9536965947eef126e67e
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-01-27 21:38:03 +0100

    Update regattastart9.py

--- regattastart9.py
+++ regattastart9.py
@@ -375,7 +375,7 @@ def finish_recording(cam, video_path, num_starts, video_end, start_time_sec):
     # fpsw = 20  # Number of frames written per second
 
     # Set duration of video1 recording
-    max_duration = (video_end + (num_starts-1)*5) * 60 
+    max_duration = (video_end + (num_starts-1)*5) * 60
     logger.debug(f"Video1, max recording duration: {max_duration} seconds")
 
     # Camera
@@ -388,11 +388,11 @@ def finish_recording(cam, video_path, num_starts, video_end, start_time_sec):
         cam.start()
 
     actual_fps = measure_frame_rate(cam)
-    fpsw = int(measure_frame_rate(cam))
+    fpsw = int(actual_fps)
     logger.info(f"Finish recording, Measured Frame Rate: {actual_fps:.2f} FPS")
 
     # Setup pre-detection parameters
-    pre_detection_duration = 5  # Seconds
+    pre_detection_duration = 6  # Seconds
     pre_detection_buffer = deque(maxlen=fpsw * pre_detection_duration)  # Automatically manages size
 
     # Load the pre-trained YOLOv5 model (e.g., yolov5s)
@@ -407,7 +407,7 @@ def finish_recording(cam, video_path, num_starts, video_end, start_time_sec):
         return
 
     # setup Post detection
-    max_post_detection_duration = 5  # Record frames during 3 sec after detection
+    max_post_detection_duration = 6  # Record frames during 6 sec after detection
     number_of_post_frames = int(fpsw * max_post_detection_duration)  # Initial setting, to record after detection
     boat_in_current_frame = False
 
