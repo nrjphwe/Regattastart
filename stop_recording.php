fd87f514f6447cb1507a6d6db39b8396e9b4eb07
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-02-18 17:04:14 +0100

    Update stop_recording.php

--- stop_recording.php
+++ stop_recording.php
@@ -7,7 +7,7 @@ $pipeHandle = fopen($pipePath, 'w');
 if ($pipeHandle === false) {
     // Handle error
     $lastError = error_get_last();
-    console_log('Stop_recording.php Failed to open named pipe: ' . $lastError['message']);
+    error_log('Stop_recording.php Failed to open named pipe: ' . $lastError['message']);
     die('Failed to open named pipe.');
 } else {
     console_log('Stop_recording.php Line 22: Opened pipe handle successfully: ' . $pipeHandle);
@@ -16,16 +16,16 @@ if ($pipeHandle === false) {
 $message = "stop_recording\n";
 if (fwrite($pipeHandle, $message) === false) {
     // Log error if writing to pipe fails
-    console_log('Stop_recording.php Line 28: Failed to write message to named pipe.');
+    error_log('Stop_recording.php Line 28: Failed to write message to named pipe.');
 } else {
     // Log success message
-    console_log('Stop_recording.php Line 31: Message sent to named pipe: ' . $message);
+    error_log('Stop_recording.php Line 31: Message sent to named pipe: ' . $message);
 }
 
 if (fclose($pipeHandle) === false) {
-    console_log('Stop_recording.php Line 38: Failed to close pipe.');
+    error_log('Stop_recording.php Line 38: Failed to close pipe.');
 } else {
     // Log success message
-    console_log('Stop_recording.php Line 38: succesfully closed the pipe:');
+    error_log('Stop_recording.php Line 38: succesfully closed the pipe:');
 }
 ?>
