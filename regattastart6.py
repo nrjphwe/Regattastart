d8c2489d938d6e3c667dc8c0cfea84731082abb1
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-03-27 11:53:16 +0100

    Update regattastart6.py

--- regattastart6.py
+++ regattastart6.py
@@ -11,11 +11,11 @@ import logging.config
 import json
 
 import subprocess
+from picamera2 import PiCamera, Color
 import RPi.GPIO as GPIO
-from picamera import PiCamera, Color
 
 # parameter data
-signal_dur = 0.9 # 0.3 sec
+signal_dur = 0.9  # 0.3 sec
 log_path = '/usr/lib/cgi-bin/'
 video_path = '/var/www/html/images/'
 photo_path = '/var/www/html/images/'
@@ -26,12 +26,14 @@ signal = 26
 lamp1 = 20
 lamp2 = 21
 
+
 def setup_logging():
     global logger  # Make logger variable global
     logging.config.fileConfig('/usr/lib/cgi-bin/logging.conf')
     logger = logging.getLogger('Start')
     logger.info("  Line  34: Start logging regattastart6")
     return logger
+
 
 def setup_camera():
     try:
@@ -40,11 +42,12 @@ def setup_camera():
         camera.framerate = 5
         camera.annotate_background = Color('black')
         camera.annotate_foreground = Color('white')
-        camera.rotation = (180) # Depends on how camera is mounted
+        camera.rotation = (180)  # Depends on how camera is mounted
         return camera  # Add this line to return the camera object
     except Exception as e:
         logger.error(f"  Line  47: Failed to initialize camera: {e}")
         return None
+
 
 def setup_gpio():
     GPIO.setmode(GPIO.BCM)
@@ -54,12 +57,14 @@ def setup_gpio():
     GPIO.setup(lamp2, GPIO.OUT, initial=GPIO.HIGH)
     return signal, lamp1, lamp2
 
+
 def remove_picture_files(directory, pattern):
     files = os.listdir(directory)
     for file in files:
         if file.endswith(pattern):
             file_path = os.path.join(directory, file)
             os.remove(file_path)
+
 
 def remove_video_files(directory, pattern):
     files = os.listdir(directory)
@@ -68,50 +73,57 @@ def remove_video_files(directory, pattern):
             file_path = os.path.join(directory, file)
             os.remove(file_path)
 
+
 def trigger_relay(port):
     if port == 'Signal':
         GPIO.output(signal, ON)
         time.sleep(signal_dur)
         GPIO.output(signal, OFF)
         time.sleep(1 - signal_dur)
-        logger.info ("  Line  78:    Trigger signal %s sec, then wait for 1 - %s sec", signal_dur, signal_dur)
+        logger.info(f"Trigger signal {signal_dur} sec, then wait for 1 - {signal_dur} sec")
     elif port == 'Lamp1_on':
         GPIO.output(lamp1, ON)
-        logger.info ('  Line  81: Lamp1_on')
+        logger.info('  Line  81: Lamp1_on')
     elif port == 'Lamp2_on':
         GPIO.output(lamp2, ON)
-        logger.info ('  Line  84: Lamp2_on')
+        logger.info('  Line  84: Lamp2_on')
     elif port == 'Lamp1_off':
         GPIO.output(lamp1, OFF)
-        logger.info ('  Line  87: Lamp1_off')
+        logger.info('  Line  87: Lamp1_off')
     elif port == 'Lamp2_off':
         GPIO.output(lamp2, OFF)
-        logger.info ('  Line  90: Lamp2_off')
+        logger.info('  Line  90: Lamp2_off')
+
 
 def capture_picture(camera, photo_path, file_name):
     camera.capture(os.path.join(photo_path, file_name), use_video_port=True)
     logger.info ("  Line 94:  Capture picture = %s ", file_name)
 
+
 def start_video_recording(camera, video_path, file_name):
     if camera.recording:
         camera.stop_recording()
     camera.start_recording(os.path.join(video_path, file_name))
-    logger.info ("  Line 100: Started recording of %s ", file_name)
+    logger.info(f'Started recording of {file_name}')
+
 
 def stop_video_recording(camera):
     camera.stop_recording()
-    logger.info ("  104: Video recording stopped")
+    logger.info("  104: Video recording stopped")
+
 
 def annotate_video_duration(camera, start_time_sec):
     time_now = dt.datetime.now()
     seconds_since_midnight = time_now.hour * 3600 + time_now.minute * 60 + time_now.second
-    elapsed_time = seconds_since_midnight - start_time_sec #elapsed since last star until now)
+    elapsed_time = seconds_since_midnight - start_time_sec  #elapsed since last star until now)
     camera.annotate_text = f"{dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Seconds since last start: {elapsed_time}"
+
 
 def convert_video_to_mp4(video_path, source_file, destination_file):
     convert_video_str = "MP4Box -add {} -new {}".format(os.path.join(video_path,source_file), os.path.join(video_path,destination_file))
     subprocess.run(convert_video_str, shell=True)
-    logger.info ("  Line 115 Video recording %s converted ", destination_file)
+    logger.info(f'Video recording {destination_file} converted')
+
 
 def start_sequence(camera, signal, start_time_sec, num_starts, dur_between_starts, photo_path):
     for i in range(num_starts):
@@ -119,7 +131,7 @@ def start_sequence(camera, signal, start_time_sec, num_starts, dur_between_start
         # Adjust the start_time_sec for the second iteration
         if i == 1:
             start_time_sec += dur_between_starts * 60  # Add 5 or 10 minutes for the second iteration
-            logger.info(f"  Line 123: Start_sequence, Next start_time_sec: {start_time_sec}")
+            logger.info(f"Start_sequence, Next start_time_sec: {start_time_sec}")
 
         # Define time intervals for each relay trigger
         time_intervals = [
@@ -151,17 +163,18 @@ def start_sequence(camera, signal, start_time_sec, num_starts, dur_between_start
                 if seconds_now == seconds:
                     # Check if the event has already been triggered for this time interval
                     if (seconds, log_message) not in last_triggered_events:
-                        logger.info(f"  Line 155: Start_sequence, Triggering event at seconds_now: {seconds_now}")
+                        logger.info(f"Start_sequence, Triggering event at seconds_now: {seconds_now}")
                         if action:
                             action()
                         picture_name = f"{i + 1}a_start_{log_message[:5]}.jpg"
                         capture_picture(camera, photo_path, picture_name)
-                        logger.info(f"  Line 160: Start_sequence, log_message: {log_message}")
-                        logger.info(f"  Line 161: Start_sequence, seconds_since_midnight: {seconds_since_midnight}, start_time_sec: {start_time_sec}")
+                        logger.info(f"Start_sequence, log_message: {log_message}")
+                        logger.info(f"Start_sequence, seconds_since_midnight: {seconds_since_midnight}, start_time_sec: {start_time_sec}")
                         # Record that the event has been triggered for this time interval
-                        logger.info(f"  Line 163: last_triggered_events = {last_triggered_events}")
+                        logger.info(f'last_triggered_events = {last_triggered_events}')
                     last_triggered_events[(seconds, log_message)] = True
         logger.info(f"  Line 164:  Start_sequence, End of iteration: {i}")
+
 
 def finish_recording(camera, video_path, video_delay, num_video, video_dur, start_time_sec):
     # Wait for finish, when the next video will start (delay)
@@ -177,17 +190,17 @@ def finish_recording(camera, video_path, video_delay, num_video, video_dur, star
 
         while (dt.datetime.now() - t2).seconds < (60 * video_dur):
             annotate_video_duration(camera, start_time_sec)
-            #camera.annotate_text = f"{dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Seconds since first start: {elapsed_time.seconds}"
+            # camera.annotate_text = f"{dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Seconds since first start: {elapsed_time.seconds}"
             camera.wait_recording(0) # was 0.5
 
         stop_video_recording(camera)
     logger.info("  Line 185: This was the last recorded video =====")
 
+
 def main():
     logger = setup_logging()  # Initialize the logger
-    camera = None # Initialize the camera variable
-    signal = None # Initialize the signal relay/variable
-    video_recording_started = False
+    camera = None  # Initialize the camera variable
+    signal = None  # Initialize the signal relay/variable
 
     # Check if a command-line argument (JSON data) is provided
     if len(sys.argv) < 2:
@@ -196,7 +209,7 @@ def main():
 
     try:
         form_data = json.loads(sys.argv[1])
-        #logger.info("form_data: %s", form_data)
+        # logger.info("form_data: %s", form_data)
         start_time = str(form_data["start_time"]) # this is the first start
         week_day = str(form_data["day"])
         video_delay = int(form_data["video_delay"])
@@ -218,7 +231,7 @@ def main():
         start_hour, start_minute = start_time.split(':')
         start_time_sec = 60 * (int(start_minute) + 60 * int(start_hour))
 
-        t5min_warning = start_time_sec - 5 * 60 # time when the start-machine should begin to execute.
+        t5min_warning = start_time_sec - 5 * 60  # time when the start-machine should begin to execute.
         wd = dt.datetime.today().strftime("%A")
 
         if wd == week_day:
@@ -244,7 +257,7 @@ def main():
                         while (dt.datetime.now() - t0).seconds < (119):
                             now = dt.datetime.now()
                             seconds_since_midnight = now.hour * 3600 + now.minute * 60 + now.second
-                            #logger.info("Inside inner loop. seconds_since_midnight=%s", seconds_since_midnight)
+                            # logger.info("Inside inner loop. seconds_since_midnight=%s", seconds_since_midnight)
                             annotate_video_duration(camera, start_time_sec)
                             camera.wait_recording(0)
 
@@ -253,20 +266,20 @@ def main():
                     # Exit the loop after the condition is met
                     break
 
-        logger.info("Line 257 Finish recording outside inner loop. start_time_sec=%s", start_time_sec)
+        logger.info(f'Finish recording outside inner loop. start_time_sec: {start_time_sec}')
         finish_recording(camera, video_path, video_delay, num_video, video_dur, start_time_sec)
 
         # convert all the videos to mp4
         stop = num_video + 1
         for i in range(1, stop):
             convert_video_to_mp4(video_path, f"video{i}.h264", f"video{i}.mp4")
-        logger.info(" Line 264 This was the last converted video =====")
+        logger.info("This was the last converted video =====")
 
     except json.JSONDecodeError as e:
-        logger.info ("Failed to parse JSON: %", str(e))
+        logger.info(f'Failed to parse JSON: str({e})')
         sys.exit(1)
     finally:
-        logger.info(" Line 270: This is finally section")
+        logger.info("This is finally section")
         if camera is not None:
             camera.close()  # Release the camera resources
         if signal is not None:
@@ -275,6 +288,7 @@ def main():
             GPIO.output(lamp2, OFF)  # Turn off the signal output
         GPIO.cleanup()
 
+
 if __name__ == "__main__":
-    #logging.basicConfig(level=logging.WARNING)  # Set log level to WARNING
+    # logging.basicConfig(level=logging.WARNING)  # Set log level to WARNING
     main()
