255373f591ea0a7be93497524a5c688c89d4a66a
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-05-18 13:04:24 +0200

    Update common_module.py

--- common_module.py
+++ common_module.py
@@ -128,6 +128,7 @@ def capture_picture(camera, photo_path, file_name, rotate=False):
                 frame = cv2.rotate(frame, cv2.ROTATE_180)
             resized_image = cv2.resize(frame, (640, 360))  # Resize to 720x480
             cv2.imwrite(os.path.join(photo_path, file_name), resized_image)
+            logger.debug(f"Saved image size: {resized_image.shape}")
             # cv2.imwrite(os.path.join(photo_path, file_name), frame)
         request.release()
         logger.info(f'Captured picture: {file_name}')
