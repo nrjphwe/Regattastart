f58d3192bcfffc2eb8adbd40bac872ac095c0bb9
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-05-20 12:11:04 +0200

    Update check_video_completion.php

--- check_video_completion.php
+++ check_video_completion.php
@@ -1,4 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: http://78.79.198.9"); // eller ange specifik domän istället för '*'
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
     include_once 'functions.php';
     // Read the content of the status file
     $status = trim(file_get_contents('/var/www/html/status.txt'));
