7fabe0442a71663a1b95e0cd52d50313ed9002b7
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2025-06-18 21:59:48 +0200

    Update index9.php

--- index9.php
+++ index9.php
@@ -206,10 +206,10 @@
                             <p></p>
                             End for the finish video, duration from last start:
                             <select name = "video_end" id = "video_end">
-                                <option value="60" <?php if(isset($video_end) && $video_end == "60"){echo "selected=\"selected\"";} ?> value="60">60</option>
-                                <option value="90" <?php if(isset($video_end) && $video_end == "90"){echo "selected=\"selected\"";} ?> value="90">90</option>
-                                <option value="120" <?php if(isset($video_end) && $video_end == "120"){echo "selected=\"selected\"";} ?> value="120">120</option>
                                 <option value="180" <?php if(isset($video_end) && $video_end == "180"){echo "selected=\"selected\"";} ?> value="180">180</option>
+                                <option value="120" <?php if(isset($video_end) && $video_end == "120"){echo "selected=\"selected\"";} ?> value="120">120</option>
+                                <option value="90" <?php if(isset($video_end) && $video_end == "90"){echo "selected=\"selected\"";} ?> value="90">90</option>
+                                <option value="60" <?php if(isset($video_end) && $video_end == "60"){echo "selected=\"selected\"";} ?> value="60">60</option>
                                 <option value="20"  value="20">20</option>
                             </select>
                             <br>
