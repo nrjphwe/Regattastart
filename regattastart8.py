d74b8413eaa0f5f8d15f88c2ed39e718e408e891
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2026-09-24 13:52:09 +0200

    Update regattastart8.py

--- regattastart8.py
+++ regattastart8.py
@@ -110,7 +110,7 @@ def load_yolov8_model(result_queue):
     try:
         start_time = time.time()
         # Sökväg till din tränade modell
-        model_path = "/var/www/html/yolov8.pt"
+        model_path = "/opt/regattastart/models/yolov8.pt""
 
         if not os.path.exists(model_path):
             logger.error(f"Model file not found at {model_path}")
